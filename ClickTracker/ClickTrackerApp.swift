99e7b14cd1aef63d5a57874dd303781b06b7c166
mizech <michael.zech@hotmail.de>
2025-03-04 12:32:09 +0100

    Initial Commit

---
+++ ClickTracker/ClickTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickTrackerApp.swift
+//  ClickTracker
+//
+//  Created by Michael on 04.03.25.
+//
+
+import SwiftUI
+
+@main
+struct ClickTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
