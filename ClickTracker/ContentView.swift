829bf7e22c69bda66d4eef681158d2c7b052b892
mizech <michael.zech@hotmail.de>
2025-03-04 12:51:57 +0100

    Added: SettingsSheet

--- ClickTracker/ContentView.swift
+++ ClickTracker/ContentView.swift
@@ -1,14 +1,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isSettingsShown = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            TabView {
+                VStack {
+                    
+                }.tabItem {
+                    Label("Clicker", systemImage: "hand.tap.fill")
+                }
+                List {
+                    
+                }.tabItem {
+                    Label("Notes", systemImage: "note.text")
+                }
+            }.toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Settings", systemImage: "gear") {
+                        isSettingsShown.toggle()
+                    }
+                }
+            }.sheet(isPresented: $isSettingsShown) {
+                Text("Settings")
+            }
         }
-        .padding()
     }
 }
 
